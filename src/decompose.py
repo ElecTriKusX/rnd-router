51ed2057e834fe72b76f7b162f8a6ebeb36a45b1
J0hnLenin <ziziz123ziziz@gmail.com>
2026-07-11 12:08:37 +0500

    Добавил pydantic

--- src/decompose.py
+++ src/decompose.py
@@ -3,28 +3,42 @@ from prompt import read_prompt, validate_json
 from dotenv import load_dotenv
 import logging
 from typing import Any
+from pydantic import BaseModel, ValidationError
+from typing import List
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
+class Subtask(BaseModel):
+    id: int
+    topic: str
+    keywords: List[str]
+
+class DecomposeResponse(BaseModel):
+    subtasks: List[Subtask]
+
 class DecomposeError(Exception):
     pass
 
-def decompose(llm: LLM, grant_description: str, prompt_version:int) -> dict:
+def decompose(llm: LLM, grant_description: str, prompt_version: int) -> DecomposeResponse:
     raw_prompt = read_prompt(f"decompose_v{prompt_version}")
     prompt = raw_prompt.replace("{grant_description}", grant_description)
 
-    response = ""
     retry_attempts = 2
     for retry in range(retry_attempts):
         response = llm.chat(prompt)
         success, data = validate_json(response)
-        if success and validate_subtasks_structure(data):
-            return data
-        logger.warning(f"Decompose failed (validation), retry {retry}: {response}")
-    
-    logger.error(f"Decompose failed after all retries ({retry_attempts})")
-    raise DecomposeError(f"Unable to get a valid JSON response for the description: {grant_description}")
+        if not success:
+            logger.warning(f"Invalid JSON received, retry {retry}: {response}")
+            continue
+
+        try:
+            return DecomposeResponse(**data)
+        except ValidationError as e:
+            logger.warning(f"Decompose failed (validation), retry {retry}: {response}")
+
+    logger.error(f"НDecompose failed after {retry_attempts} retries")
+    raise DecomposeError(f"Unable to get a valid JSON response for description: {grant_description}")
 
 def validate_subtasks_structure(data: dict) -> bool:
     # Проверяем наличие корневого ключа
@@ -81,3 +95,4 @@ if __name__ == '__main__':
 
     result = decompose(llm, test_data[0], 2)
     print(result)
+    print(type(result))
